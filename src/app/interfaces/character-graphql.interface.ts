0ec5a4d88142fbaa56fa2f50b7835a53bd6485f8
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-06-19 18:06:30 -0300

    fix: Add interface to character with Graphql

---
+++ src/app/interfaces/character-graphql.interface.ts
@@ -0,0 +1,29 @@
+import { Episode } from './episode.interface';
+import { Location } from './location.interface';
+
+export interface Resident {
+  id: string;
+  name: string;
+}
+
+export interface CharacterGraphql {
+  id: string;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: Location;
+  location: Location;
+  episode: Episode[];
+  image: string;
+  created: string;
+}
+
+export interface CharacterGraphqlData {
+  character: CharacterGraphql;
+}
+
+export interface CharacterGraphqlResponse {
+  data: CharacterData;
+}
--- src/app/services/api-graphql.service.ts
+++ src/app/services/api-graphql.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CharacterGraphqlResponse } from '../interfaces/character-graphql.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,9 @@ export class ApiGraphqlService {
 
   constructor(private http: HttpClient) {}
 
-  getCharacterFullDetailsById(id: number): Observable<any> {
+  getCharacterFullDetailsById(
+    id: number
+  ): Observable<CharacterGraphqlResponse> {
     const query = `{
       character(id: ${id}) {
         id
